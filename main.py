70b6336db76e040666f18cbcc5898a930615d41a
Ilker Balpinar <ilker.balpinar@turk.net>
2023-11-20 02:18:06 +0300

    Separate basic parameters from the application's main file

--- main.py
+++ main.py
@@ -1,48 +1,39 @@
+import parameters
+import os
 import io
 import json
-import os
 import pathlib
-import datetime
 
 '''
 pulse|ml|factor|milliliter|cl|timestamp
 '''
 
-current_date = datetime.date.today().strftime('%Y%m%d')
-START_UP_PARAMETERS_NO_ERROR = 0
-START_UP_PARAMETERS_ERROR = 1
-PATH = '~/Projects/wdspy-raspi-data/'
-FILE_NAME = 'pourings-' + current_date + '.json'
-FULL_PATH = PATH + FILE_NAME
-EXPANDED_PATH = os.path.expanduser(PATH)
-EXPANDED_FILE_NAME_PATH = os.path.expanduser(FULL_PATH)
-
 
 def startup():
     print("Please wait while the application is starting up...")
-    pathlib.Path(EXPANDED_PATH).mkdir(parents=True, exist_ok=True)
-    if os.path.isfile(EXPANDED_FILE_NAME_PATH) and os.access(EXPANDED_FILE_NAME_PATH, os.R_OK):
+    pathlib.Path(parameters.EXPANDED_PATH).mkdir(parents=True, exist_ok=True)
+    if os.path.isfile(parameters.EXPANDED_FILE_NAME_PATH) and os.access(parameters.EXPANDED_FILE_NAME_PATH, os.R_OK):
         # checks if file exists
         print("File exists and is readable.")
     else:
         print("Either file is missing or is not readable, creating file...")
-        with io.open(os.path.join(EXPANDED_PATH, FILE_NAME), 'w') as db_file:
+        with io.open(os.path.join(parameters.EXPANDED_PATH, parameters.FILE_NAME), 'w') as db_file:
             # Create a file with empty json array
             db_file.write(json.dumps([]))
-    return START_UP_PARAMETERS_NO_ERROR
+    return parameters.START_UP_PARAMETERS_NO_ERROR
 
 
 def write_file(pouring):
-    with open(EXPANDED_FILE_NAME_PATH) as fp:
+    with open(parameters.EXPANDED_FILE_NAME_PATH) as fp:
         pourings = json.load(fp)
 
     pourings.append(pouring)
 
-    with open(EXPANDED_FILE_NAME_PATH, 'w') as json_file:
+    with open(parameters.EXPANDED_FILE_NAME_PATH, 'w') as json_file:
         json.dump(pourings, json_file, indent=2, separators=(',', ': '))
 
 
-if startup() == START_UP_PARAMETERS_NO_ERROR:
+if startup() == parameters.START_UP_PARAMETERS_NO_ERROR:
     pouring = {"a": 54, "b": 87}
     write_file(pouring)
 else:
---
+++ parameters.py
@@ -0,0 +1,11 @@
+import os
+import datetime
+
+current_date = datetime.date.today().strftime('%Y%m%d')
+START_UP_PARAMETERS_NO_ERROR = 0
+START_UP_PARAMETERS_ERROR = 1
+PATH = '~/Projects/data/wdspy-raspi-data/'
+FILE_NAME = 'pourings-' + current_date + '.json'
+FULL_PATH = PATH + FILE_NAME
+EXPANDED_PATH = os.path.expanduser(PATH)
+EXPANDED_FILE_NAME_PATH = os.path.expanduser(FULL_PATH)
