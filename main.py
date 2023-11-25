9fec7f2d6ca6db534b885aa1425ab608048fd121
Ilker Balpinar <ilker.balpinar@turk.net>
2023-11-26 01:54:16 +0300

    Write real data from Raspi as a JSON object

--- main.py
+++ main.py
@@ -9,6 +9,31 @@ import FlowDataClass
 import parameters
 import utils
 
+import RPi.GPIO as GPIO
+import time, sys, datetime
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(parameters.FLOW_SENSOR_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+global count, _continue, total_pulse
+'''
+    count -> will be used to count the pulses
+    _continue -> will be used to check the cycle as long as the pulses continue
+    total_pulse -> will be used to keep track of the number of pulses in a pouring
+'''
+count = 0
+_continue = 0
+total_pulse = 0
+
+
+def countPulse(channel):
+    global count
+    if start_counter == 1:
+        count = count + 1
+
+GPIO.add_event_detect(parameters.FLOW_SENSOR_GPIO, GPIO.FALLING, callback=countPulse)
+
 
 def startup():
     print("Please wait while the application is starting up...")
@@ -25,13 +50,59 @@ def startup():
 
 
 if startup() == parameters.START_UP_PARAMETERS_NO_ERROR:
-    timestamp = calendar.timegm(time.gmtime())
-    current_datetime = time.strftime(parameters.current_datetime_format, time.gmtime(timestamp))
+    # Start of infinite loop
+    while True:
+        try:
+            start_counter = 1
+            time.sleep(parameters.PULSE_COLLECTION_INTERVAL)
+            start_counter = 0
 
-    flow_data_class = FlowDataClass.FlowData(utils.get_serial_number(), 453, 5, 550, 5, timestamp, current_datetime)
-    flow_data_str = json.dumps(flow_data_class.__dict__).replace("\\", "")
+            '''
+                seni cok seviyorum BABA!
+    
+                My 8-year-old daughter wrote this line for
+                me while I was away from the computer. So,
+                I wanted to send this line to the repository ;)
+                It means "I love you so much DAD" in Turkish <3
+            '''
 
-    utils.write_flow_data_to_disc(flow_data_str)
+            timestamp = calendar.timegm(time.gmtime())
+            current_datetime = time.strftime(parameters.current_datetime_format, time.gmtime(timestamp))
 
+            if count != 0:
+                total_pulse += count
+                # A flow data will be written to the json file for each batch pulse accumulated for half a second.
+                print("The pulse is: %.d" % count)
+                print(datetime.datetime.utcnow())
+
+                # Start of preparing flow data routine
+                flow_data_class = FlowDataClass.FlowData(utils.get_serial_number(), count, 0, 0, 0, timestamp, current_datetime)
+                flow_data_str = json.dumps(flow_data_class.__dict__).replace("\\", "")
+                utils.write_flow_data_to_disc(flow_data_str)
+                # End of preparing flow data routine
+
+                _continue = 1
+            else:
+                if _continue == 1:
+                    # When the flow is completed, it should also be written the batch pulse.
+                    print("total pulse: %.d" % total_pulse)
+                    print("----------------------")
+
+                    # Start of preparing flow data routine
+                    flow_data_class = FlowDataClass.FlowData(utils.get_serial_number(), total_pulse, 1, 1, 1, timestamp, current_datetime)
+                    flow_data_str = json.dumps(flow_data_class.__dict__).replace("\\", "")
+                    utils.write_flow_data_to_disc(flow_data_str)
+                    # End of preparing flow data routine
+
+                    _continue = 0
+                    total_pulse = 0
+
+            count = 0
+        except KeyboardInterrupt:
+            print('\nKeyboard Interrupt! Application stopped by the User.')
+            GPIO.cleanup()
+            sys.exit()
+
+    # End of infinite loop
 else:
     print('Something went wrong...')
